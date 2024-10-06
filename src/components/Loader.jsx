4f2875eea0604c2dda1eeb283eb0c31c14e9570b
shakil12 <tahmidul.work@gmail.com>
2024-10-06 11:45:57 +0600

    a simple loader component is created but not used yet

---
+++ src/components/Loader.jsx
@@ -0,0 +1,9 @@
+const Loader = () => {
+    return (
+        <div>
+            <div className="rounded-md w-12 h-12 border-4 border-t-4 border-blue-500 animate-spin absolute"></div>
+        </div>
+    );
+}
+
+export default Loader;
